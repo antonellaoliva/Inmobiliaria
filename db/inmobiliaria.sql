9d3cb86532c1420730f1bb5f5d66201cc2c5c03c
Antonella Oliva <antonellaoliva16@gmail.com>
2025-08-26 00:05:56 -0300

    cambios en la bd

--- db/inmobiliaria.sql
+++ db/inmobiliaria.sql
@@ -20,3 +20,13 @@ CREATE TABLE inquilino (
   telefono VARCHAR(20),
   email    VARCHAR(100)
 );
+
+INSERT INTO Propietario (DNI, Nombre, Apellido, Telefono, Email) VALUES
+('37528654','Lucas','Gomez','2657853698','lugomez@gmail.com'),
+('11532823','Romina','Gonzalez','2664853694','rgonza@gmail.com'),
+('23865147','Emanuel','Lopez','2657869321','ema@gmail.com');
+
+INSERT INTO Inquilino (DNI, Nombre, Apellido, Telefono, Email) VALUES
+('37523698','Amancio','Rivadera','2657253698','amanriv@gmail.com'),
+('14852321','Analia','Garcia','2657523695','anag@gmail.com'),
+('25896321','Lorenzo','Perez','2657529651','lperez@gmail.com');
